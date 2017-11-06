1b487b90770c64cd2913d7a35dd3439aedd6bc20
Kris Clemente <kristopher.clemente@gmail.com>
2017-11-06 02:30:42 -0800

    first exmaple of page objects

--- tests/first_test.js
+++ tests/first_test.js
@@ -1,11 +1,10 @@
 module.exports = {
   'Demo test Google' : function (browser) {
-    browser
-      .url('http://www.google.com')
+    browser.url('http://www.google.com')
       .waitForElementVisible('body', 1000)
       .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('input[value="Google Search"', 1000)
-      .click('input[value="Google Search"')
+      .waitForElementVisible('input[value="Google Search"]', 1000)
+      .click('input[value="Google Search"]')
       .pause(1000)
       .assert.containsText('#main', 'Night Watch')
       .end();
