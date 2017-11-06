afd9025115958180a5340658d4b43323c5837b10
Kris Clemente <kristopher.clemente@gmail.com>
2017-11-05 23:21:07 -0800

    fixed google test

--- tests/first_test.js
+++ tests/first_test.js
@@ -4,8 +4,8 @@ module.exports = {
       .url('http://www.google.com')
       .waitForElementVisible('body', 1000)
       .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('button[name=btnG]', 1000)
-      .click('button[name=btnG]')
+      .waitForElementVisible('input[value="Google Search"', 1000)
+      .click('input[value="Google Search"')
       .pause(1000)
       .assert.containsText('#main', 'Night Watch')
       .end();
